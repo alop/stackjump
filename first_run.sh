885029ffec9f52bfae989d1caa9263c96c0be7a3
alop <alopgeek@gmail.com>
2013-11-08 01:53:29 -0800

    minor changes due to berks

--- first_run.sh
+++ first_run.sh
@@ -12,8 +12,19 @@ if [ -d /root/.chef ]; then
 fi
 /root/knife_first_run
 knife configure client /etc/chef && chef-client
+apt-get -q -y update
+apt-get -q -y install ruby1.9.1
+apt-get -q -y install git
+apt-get -q -y purge ruby1.8
+gem install bundler
+cd /root/extras/chef-repo/
+bundle
+bundle exec berks upload
+knife upload roles/
+knife upload environments/
 knife cookbook upload -o /root/extras/chef-repo/cookbooks --all
 knife node run_list add $FQDN "recipe[chef-client]"
+knife node run_list add $FQDN "role[booted]"
 sleep 2
 chef-client
 sed -i 's,sh /root/first_run.sh,exit 0,' /etc/rc.local
